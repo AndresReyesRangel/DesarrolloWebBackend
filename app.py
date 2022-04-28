77ebb54dda4989c6261e4e2527f66a1fb75ca1b1
Andres R <A01746592@itesm.mx>
2022-04-28 00:17:38 -0500

    Crea usuario, verifica si existe correo y contraseña, hace logout

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask, redirect, render_template, request, session, url_for
+from flask import Flask, redirect, url_for, request, render_template, session
 import datetime
 import pymongo
-
 
 # FlASK
 #############################################################
@@ -12,42 +11,136 @@ app.secret_key = "super secret key"
 
 # MONGODB
 #############################################################
-mongodb_key = "mongodb+srv://desarrollowebuser:desarrollowebpassword@cluster0.dfh7g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+mongodb_key = "mongodb+srv://userdesarrolloweb:password_desarrolloweb@cluster0.6vflw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 client = pymongo.MongoClient(
     mongodb_key, tls=True, tlsAllowInvalidCertificates=True)
 db = client.Escuela
-cuentas = db.alumno
+cuentas = db.Alumno
 #############################################################
 
 @app.route('/')
 def home():
     email = None
-    if "email" == session:
-        email = session["email"]
-        return render_template('index.html', data = email)
-    else:
-        return render_template('login.html', data = email)
-
-
-@app.route('/signup')
-def signup():
-    name = request.form["name"]
-    email = request.form["email"]
-    password = request.form["password"]
+    if 'email' in session:
+        email = session['email']
     return render_template('index.html', error=email)
-    
 
-@app.route('/login', methods=["GET","POST"])
+
+@app.route('/login', methods=['GET'])
 def login():
-    if "email" in session:
-        return redirect(url_for("home"))
+    email = None
+    if 'email' in session:
+        email = session['email']
+        return render_template('index.html', error=email)
+
+    return render_template('login.html', error=email)
+
+
+@app.route('/login', methods=['POST'])
+def login2Index():
+    email = ""
+    if 'email' in session:
+        return render_template('index.html', error=email)
+    email = request.form['email']
+    password = request.form['password']
+    email_exist = cuentas.find_one({"correo":(email)})
+    pass_exist = cuentas.find_one({"contrasena":(password)})
+    if pass_exist != None and email_exist != None:
+        session['email'] = email
+        session['password'] = password
+        return render_template('index.html', error=email)
     else:
-        if(request.method == "GET"):
-            return render_template('login.html', error="email")
+        return "<p>Usuario o Contraseña incorrecta, regresa al login e intentelo de nuevo</p>" 
+
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    email = ""
+    if 'email' in session:
+        return render_template('index.html', error=email)
+    else:
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        session['email'] = email
+        session['password'] = password
+        session['name'] = name
+    return render_template('Create.html')
+
+
+@app.route('/logout')
+def logout():
+    if 'email' in session:
+        email = session['email']
+    session.clear()
+    return redirect(url_for('home'))
+
+
+@app.route("/usuarios")
+def usuarios():
+    cursor = cuentas.find({})
+    users = []
+    for doc in cursor:
+        users.append(doc)
+    return render_template("/Usuarios.html", data=users)
+
+
+@app.route("/insert", methods=["POST"])
+def insertUsers():
+    user = {
+        "nombre": request.form["nombre"],
+        "correo": request.form["correo"],
+        "contrasena": request.form["contrasena"],
+    }
+    try:
+        cuentas.insert_one(user)
+        return redirect(url_for("usuarios"))
+    except Exception as e:
+        return "<p>El servicio no esta disponible =>: %s %s" % type(e), e
+
+
+@app.route("/find_one/<matricula>")
+def find_one(matricula):
+    try:
+        user = cuentas.find_one({"matricula": (matricula)})
+        if user == None:
+            return "<p>La matricula %s nó existe</p>" % (matricula)
         else:
-            email = request.form["email"]
-            password = request.form["password"]
-            return render_template('index.html', error=email)
+            return "<p>Encontramos: %s </p>" % (user)
+    except Exception as e:
+        return "%s" % e
+
+
+@app.route("/delete/<nombre>")
+def delete_one(matricula):
+    try:
+        user = cuentas.delete_one({"matricula": (matricula)})
+        if user.deleted_count == None:
+            return "<p>La matricula %s nó existe</p>" % (matricula)
+        else:
+            return redirect(url_for("usuarios"))
+    except Exception as e:
+        return "%s" % e
+
+
+@app.route("/update", methods=["POST"])
+def update():
+    try:
+        filter = {"matricula": request.form["matricula"]}
+        user = {"$set": {
+            "nombre": request.form["nombre"],
+        }}
+        cuentas.update_one(filter, user)
+        return redirect(url_for("usuarios"))
+
+    except Exception as e:
+        return "error %s" % (e)
+
+
+@app.route('/create')
+def create():
+    return render_template('Create.html')
+    
 
 @app.route('/estructuradedatos')
 def prueba():
@@ -63,71 +156,3 @@ def prueba():
             }]
     })
     return render_template("home.html", data=nombres)
-
-@app.route('/usuarios')
-def usuarios():
-    cursor = cuentas.find({})
-    users = []
-    for doc in cursor:
-        users.append(doc)
-    return render_template("/usuarios.html", data = users)
-
-@app.route('/insert', methods=["POST"])
-def insertUsers():
-    user = {
-        "matricula":"A01746592",
-        "nombre":"Andres Reyes",
-        "correo":"A01746592@tec.mx",
-        "contrasena":"1234567"
-    }
-
-    try:
-        cuentas.insert_one(user)
-        return redirect(url_for("usuarios"))
-    except Exception as e:
-        return "<p>El servicio no está disponible: %s %s<p>" % type(e), e
-    
-@app.route('/find_one/<matricula>')
-def find_one(matricula):
-    try:
-        user = cuentas.find_one({"matricula":(matricula)})
-        if user == None:
-            return "<p>La matricula %s nó existe</p>" % (matricula)
-        else:
-            return "<p>Encontramos: %s </p>" % (user)
-    except Exception as e:
-        return "%s" % e
-
-@app.route('/delete/<matricula>')
-def delete_one(matricula):
-    try:
-        user = cuentas.delete_one({"matricula":(matricula)})
-        if user.deleted_count == None:
-            return "<p>La matricula %s nó existe</p>" % (matricula)
-        else:
-            return redirect(url_for("usuarios"))
-
-    except Exception as e:
-        return "%s" % e
-
-
-@app.route('/update', methods=["POST"])
-def update():
-    try:
-        filter = {"matricula": request.form["matricula"]}
-        user = {"$set": {
-            "nombre": request.form["nombre"],
-            "constrasena": request.form["contrasena"]
-            }}
-        cuentas.update_one(filter, user)
-        return redirect(url_for("usuarios"))
-
-    except Exception as e:
-        return "error %s" % (e)
-
-@app.route('/create')
-def create():
-    
-    return render_template("/CreateForm.html")
-
-    
